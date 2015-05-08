7ebcf83714377c6e55edca57519e9efa7f23de9b
Milton Madanda <madandat@gmail.com>
2015-05-08 13:40:33 +0200

    update translations

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-virtualenv ve
-source ve/bin/activate
-git pull
-pip install -e .
-pip install --upgrade elastic-git
+#virtualenv ve
+#source ve/bin/activate
+#git pull
+#pip install -e .
+#pip install --upgrade elastic-git
 echo 'Done installing requirements.'
 echo 'Cloning repo..'
 rm -rf repo/
-git clone https://github.com/universalcore/unicore-cms-content-mama-tz-qa repo
+git clone https://github.com/universalcore/unicore-cms-content-mama-bd-prod repo
 echo 'Creating indexes..'
 eg-tools resync -c development.ini -m unicore.content.models.Category -f mappings/category.mapping.json -r true
 eg-tools resync -c development.ini -m unicore.content.models.Page -f mappings/page.mapping.json
--- extract_i18n.sh
+++ extract_i18n.sh
@@ -4,10 +4,7 @@ mkdir -p unicorecmsaponjon/locale
 
 pot-create -o unicorecmsaponjon/locale/unicorecmsaponjon.pot unicorecmsaponjon/
 
-declare -a arr=(
-    "eng_GB" "tha_TH" "ind_ID" "swa_TZ" "swa_KE" "fre_FR" "spa_AR" "spa_CO"
-    "spa_MX" "hin_IN" "por_PT" "mal_IN" "guj_IN" "tel_IN" "tam_IN" "mar_IN"
-    "ben_IN" "ben_BD" "fil_PH")
+declare -a arr=("eng_GB" "ben_BD")
 
 for lang in "${arr[@]}"
 do
